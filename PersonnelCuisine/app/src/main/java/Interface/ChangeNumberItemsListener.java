c01d2f3bf79798e675c6ee86e2312a4246c07b5e
MUSA-19542 <150328978+MUSA-19542@users.noreply.github.com>
2024-03-16 13:50:24 +0500

    Delete PersonnelCuisine directory

--- PersonnelCuisine/app/src/main/java/Interface/ChangeNumberItemsListener.java
+++
@@ -1,5 +0,0 @@
-package Interface;
-
-public interface ChangeNumberItemsListener {
-    void changed();
-}
--- PersonnelCuisine/app/src/main/java/Interface/FoodListCallback.java
+++
@@ -1,9 +0,0 @@
-package Interface;
-
-import com.example.personnelcuisine.Activity.FoodDataModel;
-
-import java.util.List;
-
-public interface FoodListCallback {
-    void onFoodListReceived(List<FoodDataModel> foodList);
-}
